ef3515023011ebe455399cd6d67dc59d30ab2363
denverjose <dnvrjose6@gmail.com>
2026-02-23 08:22:39 +0800

    changed initial message of chatbot

--- lib/constants/chatbot-constants.tsx
+++ lib/constants/chatbot-constants.tsx
@@ -1,18 +1,6 @@
-import { services } from "./services-constants";
-
 const BOT_NAME = "TPX";
 
-const servicesListHtml = services
-  .map(
-    (service) =>
-      `<li style="margin-left: 20px;" class="list-disc">${service.title}</li>`,
-  )
-  .join("");
-
-export const initialMessage = `
-    Hi, I'm TPX's Assistant! I'm here to answer your
-    <ul>${servicesListHtml}</ul>
-    or any other related services questions. How can I help you today?
-`;
+export const initialMessage =
+  "This is a website Denver did for his previous US client/ceo/boss, using next.js, typescript, tailwind, and framer-motion. Current chatbot not working as it needed payment to openai, but the code is there. The chatbot is designed to answer questions about the services offered by the company, and to provide information about the company itself. The chatbot is also designed to be able to answer questions about the company's history, mission, and values. ";
 
 export const botName = `${BOT_NAME} Assistant`;
